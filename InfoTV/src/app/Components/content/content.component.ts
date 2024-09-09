a903ccd40f94fecfb66e9442acfdf4e0332d6cb8
Manuel Ibañez Hacha <manuel.ibanez.external@atos.net>
2024-09-09 16:09:05 +0200

    Implemented Modal for API Key

--- InfoTV/src/app/Components/content/content.component.ts
+++ InfoTV/src/app/Components/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-content',
@@ -11,10 +12,30 @@ import { Component } from '@angular/core';
 export class ContentComponent {
 
 url:string = '6d6e96f0'
+key = ''
+
 
 ngOnInit() {
-  console.log(this.url)
+  if (localStorage.getItem('savedkey')) {
+    
+  }
+  else {
+    this.getKey()
+  }
+  
 }
+async getKey() {
+  const { value: key } = await Swal.fire({
+    title: 'Enter your OMDb API Key.',
+    input: 'text',
+    inputPlaceholder: 'Enter your key',
+    confirmButtonColor: "#1F2937"
+  });
 
+  if (key) {
+    this.key = key
+    localStorage.setItem('savedkey',this.key)
+  }
+}
 }
 
